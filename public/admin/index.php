77ea340a8cee668033094067c2859b082b34fb28
KatowProject <naufalfaqih443@gmail.com>
2022-11-30 17:17:36 +0700

    removed useless files

--- public/admin/index.php
+++ public/admin/index.php
@@ -78,9 +78,8 @@ $count = $controller->get_count_words();
     </div>
   </div>
 
-  <script src="/assets/js/jquery.min.js"></script>
-  <script src="/assets/js/popper.js"></script>
-  <script src="/assets/js/bootstrap.min.js"></script>
+  <script src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
   <script src="/assets/js/main.js"></script>
 </body>
 
