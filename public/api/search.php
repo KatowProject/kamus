20fd826454264d5c53adf6ee14caa4c06c07758e
M. Naufal Faqih <naufalfaqih443@gmail.com>
2024-02-07 22:44:03 +0700

    partial update

--- public/api/search.php
+++ public/api/search.php
@@ -7,11 +7,11 @@ require_once '../../controllers/controllers.php';
 
 $controller = new Controller();
 
-if (isset($_GET['word']) && isset($_GET['type'])):
+if (isset($_GET['word']) && isset($_GET['type'])) :
     $word = $_GET['word'];
     $type = $_GET['type'];
 
-    if (!array_key_exists($type, $controller->type)):
+    if (!array_key_exists($type, $controller->type)) :
         // set status code
         http_response_code(400);
         echo json_encode([
@@ -24,14 +24,26 @@ if (isset($_GET['word']) && isset($_GET['type'])):
     $sentences = [];
     $word = $word ? trim($_GET['word']) : '';
 
+    // check is js script or HTML
+    if (preg_match('/<script>/', $word) || preg_match('/<html>/', $word)) :
+        // set status code
+        http_response_code(400);
+        echo json_encode([
+            'status' => 'error',
+            'message' => 'Bad request'
+        ]);
+
+        exit;
+    endif;
+
     $words = $controller::splitRemoveSpecialChars($word);
-    foreach ($words as $key => $word):
+    foreach ($words as $key => $word) :
         $get_word = $controller->get_word($word, $type);
         $w[] = $get_word['translated'];
 
-        if (in_array($get_word, $sentences)):
+        if (in_array($get_word, $sentences)) :
             continue;
-        else:
+        else :
             $sentences[] = $get_word;
         endif;
     endforeach;
@@ -42,7 +54,7 @@ if (isset($_GET['word']) && isset($_GET['type'])):
             'sentences' => $sentences
         ]
     ]);
-else:
+else :
     // set status code
     http_response_code(400);
     echo json_encode([
