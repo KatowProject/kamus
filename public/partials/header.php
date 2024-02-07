fa53665aeb24fca4eed7f2942ec10ef6cb9b46ee
M. Naufal Faqih <naufalfaqih443@gmail.com>
2024-02-07 20:49:37 +0700

    fix bug

--- public/partials/header.php
+++ public/partials/header.php
@@ -7,10 +7,9 @@ defined("BASEPATH") or exit("No direct script access allowed");
         <div class="container">
             <a class="navbar-brand" href="#">
                 <img src="/assets/img/logo.png" width="35" height="35" alt="logos">
-                SA<span>TECH</span>
+                KAMUS<span>SUNDA</span>
             </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
 
