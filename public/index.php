fa0e2be9139939bedd96a493e22d67f3c7357304
M. Naufal Faqih <naufalfaqih443@gmail.com>
2024-02-07 20:08:26 +0700

    partial update

--- public/index.php
+++ public/index.php
@@ -1,26 +1,26 @@
 <?php
 define('BASEPATH', 'public');
 
-require_once '../scraper.php';
-require_once '../database/database.php';
-require_once "../controllers/controllers.php";
+require_once 'scraper.php';
+require_once 'database/database.php';
+require_once "controllers/controllers.php";
 
 $controller = new Controller();
 
 try {
     $sentences = [];
-    if (isset($_POST['submit'])):
+    if (isset($_POST['submit'])) :
         $word = isset($_POST['word']) ? trim($_POST['word']) : '';
         $type = $_POST['type'];
 
         $words = $controller::splitRemoveSpecialChars($word);
-        foreach ($words as $key => $word):
+        foreach ($words as $key => $word) :
             $get_word = $controller->get_word($word, $type);
             $w[] = $get_word['translated'];
 
-            if (in_array($get_word, $sentences)):
+            if (in_array($get_word, $sentences)) :
                 continue;
-            else:
+            else :
                 $sentences[] = $get_word;
             endif;
 
@@ -43,7 +43,7 @@ try {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="assets/css/main.css" rel="stylesheet">
-    <link rel="icon" href="/assets/img/logo.png" type="image/png">
+    <link rel="icon" href="assets/img/logo.png" type="image/png">
     <title>Translate • SATECH</title>
 </head>
 
@@ -61,8 +61,7 @@ try {
                     <form action="" method="POST">
                         <div class="mb-3">
                             <label for="word" class="form-label">Word</label>
-                            <input type="text" class="form-control" id="word" name="word" placeholder="Enter word"
-                                value="<?= $_POST['word'] ?? '' ?>" required>
+                            <input type="text" class="form-control" id="word" name="word" placeholder="Enter word" value="<?= $_POST['word'] ?? '' ?>" required>
                         </div>
                         <div class="mb-3">
                             <label for="type" class="form-label">Type</label>
@@ -81,8 +80,7 @@ try {
                             </button>
 
                             <!-- button help -->
-                            <button type="button" class="btn btn-secondary" data-bs-toggle="modal"
-                                data-bs-target="#exampleModal">
+                            <button type="button" class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#exampleModal">
                                 <i class="fa fa-question-circle"></i> Help
                             </button>
                         </div>
@@ -90,77 +88,77 @@ try {
                 </div>
             </div>
 
-            <?php if ($sentences != null): ?>
-            <div class="row py-5">
-                <div class="col-12 py-3">
-                    <h2 class="text-center"><b>Result</b></h2>
-                </div>
+            <?php if ($sentences != null) : ?>
+                <div class="row py-5">
+                    <div class="col-12 py-3">
+                        <h2 class="text-center"><b>Result</b></h2>
+                    </div>
 
-                <div class="col-12">
-                    <div class="card mb-3" style="max-width: auto;">
-                        <div class="card-header bg-secondary text-white">
-                            <?= $_POST['type'] == "sih" ? "Sunda" : "Indonesia" ?>
-                        </div>
+                    <div class="col-12">
+                        <div class="card mb-3" style="max-width: auto;">
+                            <div class="card-header bg-secondary text-white">
+                                <?= $_POST['type'] == "sih" ? "Sunda" : "Indonesia" ?>
+                            </div>
 
-                        <div class="card-body bg-white">
-                            <p class="card-text">
-                                <?php
-                echo $_POST['word'];
-                                ?>
-                            </p>
+                            <div class="card-body bg-white">
+                                <p class="card-text">
+                                    <?php
+                                    echo $_POST['word'];
+                                    ?>
+                                </p>
+                            </div>
                         </div>
                     </div>
-                </div>
 
-                <div class="col-12">
-                    <div class="card mb-3" style="max-width: auto;">
-                        <div class="card-header bg-secondary text-white">
-                            <?= $_POST['type'] == "sih" ? "Indonesia" : "Sunda" ?>
+                    <div class="col-12">
+                        <div class="card mb-3" style="max-width: auto;">
+                            <div class="card-header bg-secondary text-white">
+                                <?= $_POST['type'] == "sih" ? "Indonesia" : "Sunda" ?>
 
-                        </div>
-                        <div class="card-body bg-white">
-                            <p class="card-text">
-                                <?php foreach ($w as $key => $value)
-                    echo $value . " "; ?>
-                            </p>
+                            </div>
+                            <div class="card-body bg-white">
+                                <p class="card-text">
+                                    <?php foreach ($w as $key => $value)
+                                        echo $value . " "; ?>
+                                </p>
+                            </div>
                         </div>
                     </div>
+
+                    <div class="col-12">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Word</th>
+                                    <th scope="col">Translation</th>
+                                    <th scope="col">Type</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php foreach ($sentences as $key => $value) : ?>
+                                    <tr>
+                                        <td>
+                                            <?php
+
+                                            echo $value['word'] ?? $value['name'] . " ";
+                                            ?>
+                                        </td>
+                                        <td>
+                                            <?php
+
+                                            echo $value['translated'];
+
+                                            ?>
+                                        </td>
+                                        <td>
+                                            <?= $value['lang'] ?>
+                                        </td>
+                                    </tr>
+                                <?php endforeach; ?>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
-
-                <div class="col-12">
-                    <table class="table">
-                        <thead>
-                            <tr>
-                                <th scope="col">Word</th>
-                                <th scope="col">Translation</th>
-                                <th scope="col">Type</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php foreach ($sentences as $key => $value): ?>
-                            <tr>
-                                <td>
-                                    <?php
-
-                    echo $value['word'] ?? $value['name'] . " ";
-                                    ?>
-                                </td>
-                                <td>
-                                    <?php
-
-                    echo $value['translated'];
-
-                                    ?>
-                                </td>
-                                <td>
-                                    <?= $value['lang'] ?>
-                                </td>
-                            </tr>
-                            <?php endforeach; ?>
-                        </tbody>
-                    </table>
-                </div>
-            </div>
             <?php endif; ?>
         </div>
     </main>
@@ -168,7 +166,7 @@ try {
     <!-- fixed bottom footer -->
     <footer class="text-lg-start bg-light">
         <div class="text-center p-3">
-            Copyright &copy; 2022 - IF21B SA<span>TECH</span>
+            Copyright &copy; 2024 SA<span>TECH</span>
         </div>
     </footer>
 
@@ -207,8 +205,8 @@ try {
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 
     <script>
-        $(document).ready(function () {
-            $('#submit').click(function () {
+        $(document).ready(function() {
+            $('#submit').click(function() {
                 var word = $('#word').val();
                 var type = $('#type').val();
                 if (word == '' || type == '') {
